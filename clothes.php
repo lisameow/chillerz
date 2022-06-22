ae68023c7d9217233c73befaba402e3dfb46303b
lisa <liza@endel.io>
2022-06-22 23:49:58 +0400

    logic of button cloks and local storage in prod

--- clothes.php
+++ clothes.php
@@ -12,11 +12,22 @@ $result = mysqli_query($induction, 'SELECT * FROM `catalog_clothes`');
     <link rel="stylesheet" href="/styles.css">
 	<title>clothes</title>
 </head>
+
 <body bgcolor="#FF69B4">
     <p class="no_war">нет войне! stop the war!</p>
     <div>
         <img class="marginauto" src="images/logo_6.svg"/>
     </div>
+
+    <script>
+        let item = [];
+        function save_to_storage(id)
+        {
+            document.getElementById(id).disabled = 'true';
+            item.push(id);
+            localStorage.setItem('basket_clothes', item);
+        }
+    </script>
 
     <?php
         while($good = mysqli_fetch_assoc($result)) 
@@ -29,7 +40,7 @@ $result = mysqli_query($induction, 'SELECT * FROM `catalog_clothes`');
                     <p class="title"> <?php echo $good['title']; ?> </p>
                     <p class="description"> <?php echo $good['description']; ?> </p>
                     <p class="size_price">[ <?php echo $good['size']; ?> ]<br>[ <?php echo $good['price']; ?> ₾ ]</p>
-                    <button class="buy_button">🤩 💸 🛍</button>
+                    <button class="buy_button" id="<?php echo $good['id']; ?>" onclick="save_to_storage(<?php echo $good['id']; ?>);">🤩 💸 🛍</button>
                 </div>
             </div>
             <?php 
